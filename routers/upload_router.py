e791fe8e963f7aa5bcdbcf85d8b7f22bb2dcba75
SeonhoKim <sleepygloa@gmail.com>
2025-01-31 17:16:15 +0900

    update upload_router.py url

--- routers/upload_router.py
+++ routers/upload_router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
 import os
 from utils.file_manager import save_uploaded_file
 
 router = APIRouter()
 
-@router.post("/upload_voice/")
-async def upload_voice(user_id: str, file: UploadFile = File(...)):
+@router.post("/upload_voice")  # ✅ 슬래시 `/` 제거
+async def upload_voice(user_id: str = Form(...), file: UploadFile = File(...)):
     file_path = save_uploaded_file(user_id, file)
     return {"message": "파일 업로드 완료", "file_path": file_path}
